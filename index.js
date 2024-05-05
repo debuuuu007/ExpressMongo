edf54ed51b6bb9b72a29fdb9f51d96658845b433
DEBRAJ MONDAL <debraj.abv@gmail.com>
2024-05-05 18:26:46 +0530

    CRUD app completed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express=require("express")
 const app=express();
 const path=require("path")
 
+const methodOverride = require('method-override')
+
 const chats=require("./models/chats");
 const Chat = require('./models/chats');
 
@@ -12,6 +14,7 @@ app.set("views",path.join(__dirname,"views"))
 
 app.use(express.static(path.join(__dirname,"public")))
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('X-HTTP-Method-Override'))
 
 main().then(
     ()=>{
@@ -63,5 +66,31 @@ app.post("/chats",async (req,res)=>{
         at:new Date()
     })
     await newChat.save();
+    res.redirect("/chats")
+})
+
+
+app.get("/chats/:id/edit",async (req,res)=>{
+    let{id}=req.params
+    let mes= await chats.findById(id)
+    console.log(mes)
+    res.render("edit.ejs",{mes})
+})
+
+app.post("/chats/:id", async(req,res)=>{
+    let {id}= req.params
+    let {message:newmessage}=req.body
+    // console.log(newmessage)
+
+    let newchat=await Chat.findByIdAndUpdate(id,{message:newmessage},{new:true},{runValidators:true})
+    // console.log(newchat)
+    res.redirect("/chats")
+
+})
+
+app.post("/chats/:id/rem",async (req,res)=>{
+    let {id}=req.params
+    let newchat=await Chat.findByIdAndDelete(id)
+    console.log(newchat)
     res.redirect("/chats")
 })
